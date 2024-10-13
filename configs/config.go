f4e132dc798b9b7f9d1817c84c54a711ad0fe3b5
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-10-13 17:52:13 -0300

    chore: created some configs and endpoints
    - Created the connection to the PostgreSQL database
    - Created the endpoint teacher, but just for modelling and start building other endpoints like that one

---
+++ configs/config.go
@@ -0,0 +1,35 @@
+package configs
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPassword, dbName, dbSSLMode)
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Error pinging the database: ", err)
+	}
+
+	log.Println("Successfully connected to the database!")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SC-Cynex/cynex-class-service
 
 go 1.22.7
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
