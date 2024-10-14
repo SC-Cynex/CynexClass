97c64c63b90fb7efb4712986d4d887000e3ed0ed
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-10-13 22:13:39 -0300

    fix: added godotenv loader to main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,27 @@
-// cmd/app/main.go
 package main
 
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := mux.NewRouter()
 
-	log.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	PORT := os.Getenv("APP_PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+	log.Println("Server running on port", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, r))
 }
